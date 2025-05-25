80723bacb35d870a5e2a95df6371b6a50db7a9ec
Stefan Legg <legg.stefan@gmail.com>
2025-05-25 11:11:33 -0400

    Add logo images on projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,30 +1,37 @@
+import ShopifyLogo from "@/components/logos/Shopify.astro";
+
 export type Project = {
   title: string;
   techs: string[];
   link: string;
   isComingSoon?: boolean;
+  logo?: Function | string;
 };
 
 const projects: Project[] = [
   {
-    title: "Shopify: Web Admin",
+    title: "Shopify Admin",
     techs: ["React", "TypeScript", "Rails"],
     link: "https://shopify.com",
+    logo: ShopifyLogo,
   },
   {
-    title: "Shopify: Cart Transform Function API",
+    title: "Cart Transform Function API",
     techs: ["TypeScript", "Rust"],
     link: "https://shopify.dev/docs/api/functions/latest/cart-transform",
+    logo: ShopifyLogo,
   },
   {
-    title: "Shopify: Polaris",
+    title: "Polaris",
     techs: ["React", "TypeScript"],
     link: "https://polaris.shopify.com/",
+    logo: ShopifyLogo,
   },
   {
-    title: "Lenovo: LanSchool Air",
+    title: "LanSchool Air",
     techs: ["TypeScript", "Angular"],
     link: "https://lanschool.com/solutions/lanschool-air",
+    logo: "/logos/lanschool.png",
   },
 ];
 
